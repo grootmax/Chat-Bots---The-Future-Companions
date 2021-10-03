68146614f392d7b31efdd0c9758da0294233b909
grootmax <58818000+grootmax@users.noreply.github.com>
2021-10-03 19:46:29 +0530

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,51 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } 
+    else if (input == "Hi") {
+        return "Hi there!";
+
+    } else if (input == "hi") {
+        return "hi there!";
+
+    } else if (input == "type something....") {
+        return "haha!";
+
+    } else if (input == "love") {
+        return "your are just a piece of love! Have a nice day";
+
+    } else if (input == "chennai") {
+        return  "https://bookaa.amadeus.com/plnext/AADX/Override.action#/FPOW";
+
+    } else if (input == "chen") {
+        return ; 
+
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } 
+    
+    
+    else {
+        return "Try asking something else!";
+    };
+
+    
+}
